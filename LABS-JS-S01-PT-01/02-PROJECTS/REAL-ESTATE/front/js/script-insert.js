27dd150e0d6b4825d10d603c6faca55f0f1d73d1
RDK <photocromatic@gmail.com>
2024-08-03 17:48:04 -0600

    LABs

--- LABS-JS-S01-PT-01/02-PROJECTS/REAL-ESTATE/front/js/script-insert.js
+++ LABS-JS-S01-PT-01/02-PROJECTS/REAL-ESTATE/front/js/script-insert.js
@@ -1,6 +1,23 @@
+var formUI = document.getElementsByTagName( "form" )[ 0 ];
+var inputsUI = formUI.getElementsByTagName( "input" );
+var inputId 	   = inputsUI[ 0 ]; 
+var inputAvailable = inputsUI[ 1 ]; 
+var inputProperty  = inputsUI[ 2 ]; 
+var inputLocation  = inputsUI[ 3 ]; 
+var inputPrice 	   = inputsUI[ 4 ]; 
+var inputSubmit	   = inputsUI[ 5 ]; 
+
 var dataProperty = "House EAR 2024";
 var dataLocation = true;
 var dataPrice = 3700;
+
+inputSubmit.addEventListener( "click", function( e ) {
+	e.preventDefault();
+	console.log( inputProperty.value );
+	console.log( inputLocation.value );
+	console.log( inputPrice.value );
+});
+
 
 var holder = document.getElementById( "holder" );
 var endpoint = "http://localhost:8080/index-insert.php";
