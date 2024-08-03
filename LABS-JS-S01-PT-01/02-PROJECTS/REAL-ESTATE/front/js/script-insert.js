26f3b887ca8925b99f27eb16e6dddee4778f11aa
RDK <photocromatic@gmail.com>
2024-08-03 16:48:12 -0600

    LABs

--- LABS-JS-S01-PT-01/02-PROJECTS/REAL-ESTATE/front/js/script-insert.js
+++ LABS-JS-S01-PT-01/02-PROJECTS/REAL-ESTATE/front/js/script-insert.js
@@ -1,25 +1,23 @@
-var holder = document.getElementById( "holder" );
-var endpoint = "http://localhost:8080/index-select.php";
-var htmlData = "";
-var xhr = new XMLHttpRequest();
-xhr.open( "GET", endpoint, true );
-xhr.send();
-xhr.addEventListener( "load", loadSelectedData );
+var dataProperty = "House EAR 2024";
+var dataLocation = true;
+var dataPrice = 3700;
 
-function loadSelectedData( e ) {
-	var dataJson = JSON.parse( e.target.responseText ).data;
-	dataDOMInsert( dataJson );
+var holder = document.getElementById( "holder" );
+var endpoint = "http://localhost:8080/index-insert.php";
+// var htmlData = "";
+var xhr = new XMLHttpRequest();
+xhr.open( "POST", endpoint, true );
+xhr.setRequestHeader( "Content-Type", "application/x-www-form-urlencoded" );
+xhr.send( "property=" + dataProperty + "&location=" + dataLocation + "&price=" + dataPrice );
+xhr.addEventListener( "load", loadInsertedData );
+
+function loadInsertedData( e ) {
+	var dataJson = JSON.parse( e.target.responseText );
+	// dataDOMInsert( dataJson );
+	console.log( dataJson );
 }
 
 function dataDOMInsert( arrayData ) {
-	for( var i = 0, l = arrayData.length; i < l; i ++ ) {
-		htmlData += "<div>" +
-			"<h2>" + arrayData[ i ].id + " - " + arrayData[ i ].property + "</h2>" +
-			"<p>Location: " + arrayData[ i ].location + "  </p>" +
-			"<p>Price: " + arrayData[ i ].price + "</p>" +
-		"</div>";
-	}
-	holder.innerHTML = htmlData;
 } 
 
 
